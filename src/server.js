1bb071288cb1b8be64a4a28a32d505fc4615f11f
Emiliano Debenedetti <emilianodebenedetti@outlook.com>
2022-09-07 22:59:59 -0300

    Implementación de login por formulario

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 import express from "express";
+/* Cookie */
+import cookieParser from "cookie-parser";
+/* Sessions */
+import session from "express-session";
+/* -----Persistencia por mongoDB----- */
+import MongoStore from "connect-mongo";
+/* mongo Atlas */
+const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true}
+
 const app = express();
+
 
 //  Firebase 
 
@@ -31,6 +41,75 @@ app.use("/api/productos", routerProducts);
 import routerCart from "./routes/cart.routes.js";
 app.use("/api/carrito", routerCart);
 
+// <--------------- Cookies -------------------->
+
+/* app.use(cookieParser()) */
+/* app.use(cookieParser('my-secret')) */ // secret 
+/* app.use(cookieParser(['my-secret', 'another-secret'])) */ // secret con array
+
+/* --------------------desafio clase -------------------------*/
+app.use(cookieParser('desafioClase'))
+app.use(express.json())
+ 
+app.post('/cookies'), (req, res) => {
+	const { nombre, valor, tiempo } = req.body
+	console.log(nombre, valor, tiempo)
+	
+	//param recibido invalido
+	if (!nombre || !valor) {
+		return res.json({ error: 'falta nombre o valor1'})
+	}
+	
+	//si no hay tiempo, generar cookie sin expiracuión
+	if (tiempo) {
+		res.cookie(nombre, valor, { signed: true, maxAge: 1000 * parseInt(tiempo) })
+	} else { 
+		res.cookie(nombre, valor, { signed: true })
+	}
+	//si sale bien:
+	res.json({ proceso: 'ok'})
+}
+
+app.get('/cookies', (req, res) => {
+	console.log(req.cookies)
+	res.json({ normales: req.cookies, firmadas: req.signedCookies })
+})
+
+app.delete('/cookies/:nombre', (req, res) => {
+	const { nombre } = req.params
+	console.log(req.cookies[nombre])
+	console.log(req.signedCookies[nombre])
+	if (!req.cookies[nombre] && !req.signedCookies[nombre]) {
+		res.json({ erroe: 'nombre de cookie invalido' })
+	} else {
+		res.clearCookie(nombre)
+		res.json({ proceso: 'ok' })
+	}
+})
+ //obtener cookie con cierto nombre
+app.get('/cookies/:nombre', (req, res) => {
+	const { nombre } = req.params
+	let name = req.cookies[nombre];
+	console.log(name)
+	res.json({ proceso: name })
+});
+/* -----------------fin ej clase -------------- */
+
+/* Sessions */
+app.use(session({
+	/* Persistencia por mongoDB */
+	store: MongoStore.create({ 
+		mongoUrl: 'mongodb+srv://localhost/',
+		mongoOptions: advancedOptions 
+	}),
+
+	secret: 'secreto',
+	resave: false,
+	saveUninitialized: false/* ,
+	cookie: {
+		maxAge: 10000
+	} */
+}))
 // Error 404 
 
 function error404(req, res, next) {
